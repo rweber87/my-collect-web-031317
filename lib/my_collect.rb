6fc393d5490327735ab411d26984d66b21565230
Robert Weber <rob.weber87@gmail.com>
2017-02-10 15:45:15 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
-
-
+def my_collect(array)
+  new_array = []
+  i = 0
+  while i < array.length
+    if array[i].split(" ").length > 1
+      new_array << array[i].split(" ")[0]
+      yield array[i]
+      i += 1
+    else
+      new_array << array[i].upcase
+      yield array[i]
+      i += 1
+    end
+  end
+  new_array
+end
